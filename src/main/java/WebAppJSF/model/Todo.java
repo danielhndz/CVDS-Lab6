dd25a29c93dd0be7f39c828130f3a99d98c0efea
Daniel <danielhndz@protonmail.com>
2020-09-21 23:07:46 -0500

    Lab 5 completed

---
+++ src/main/java/WebAppJSF/model/Todo.java
@@ -0,0 +1,29 @@
+package WebAppJSF.model;
+
+public class Todo {
+
+    private int userId;
+    private int id;
+    private String title;
+    private boolean completed;
+
+    public Todo() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+}
